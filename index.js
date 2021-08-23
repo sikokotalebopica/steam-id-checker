3ba377d886b6a1c367cc66b283e51b6c3accf777
selbstmord <67484724+selbstmord@users.noreply.github.com>
2021-08-23 22:55:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ let ids = fs.readFileSync('input.txt', 'UTF-8').split(/\r?\n/);
 console.log(`found ${ids.length} ids fuck you nigger`)
 
 let mode = true; // false = profile ids / true = group ids
+let threads = 50;
 
-for(let i = 0; i < 50; i++) {
+for(let i = 0; i < threads; i++) {
     let data = {
-        ids : ids.splice(0, ids.length / 50),
+        ids : ids.splice(0, ids.length / threads),
         mode: mode
     }
     new Worker("./check.js", { workerData: data });
